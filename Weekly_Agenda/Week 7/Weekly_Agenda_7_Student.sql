f67fe50535f8a143019688c9754d6bfdbf639570
Hüseyin Yildiz <101867736+yildiz3@users.noreply.github.com>
2022-06-14 00:26:45 +0200

    Add files via upload

---
+++ Weekly_Agenda/Week 7/Weekly_Agenda_7_Student.sql
@@ -0,0 +1,16 @@
+---- RDB&SQL Exercise-2 Student
+
+----1. By using view get the average sales by staffs and years using the AVG() aggregate function.
+
+
+----2. Select the annual amount of product produced according to brands (use window functions).
+
+
+----3. Select the least 3 products in stock according to stores.
+
+
+----4. Return the average number of sales orders in 2020 sales
+
+
+----5. Assign a rank to each product by list price in each brand and get products with rank less than or equal to three.
+
